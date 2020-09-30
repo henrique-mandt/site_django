66cada8e6ab1df07c5b03c4798b1d426d5790475
Henrique Mandt <henrique.mandt@outlook.com>
2020-09-30 09:30:58 -0300

    nao oficial

--- meu_site/blog/admin.py
+++ meu_site/blog/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish_date', 'status')
+    list_filter = ('status', 'created_on', 'publish_date', 'author')
+    date_hierarchy = 'publish_date'
+    raw_id_fields = ('author',)
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug':('title',)}
