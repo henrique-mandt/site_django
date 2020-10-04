2d3c8b0c77daef007ae7faec2014926811b465db
Henrique Mandt <henrique.mandt@outlook.com>
2020-10-04 11:26:02 -0300

    Organizaçao pastas e CSS

--- meu_site/blog/models.py
+++ meu_site/blog/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='publicado')
+
 class Post(models.Model):
     STATUS = [
         ('rascunho','Rascunho'),
@@ -21,6 +25,9 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
     choices=STATUS,
     default='rascunho')
+
+    objects     = models.Manager()
+    published   = PublishedManager()    
 
     class Meta:
         ordering = ('-publish_date',)
